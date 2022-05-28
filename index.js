4a1b1cefc1730fcf1eb94e1dab690289403ee547
andtrno1 <andtrno1@gmail.ccom>
2022-05-28 14:49:44 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    let cap = string.charAt(0).toUpperCase()
+    return cap
+  }
+
+  static sanitize(string){
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titlelize(string){
+    let exeptions = ['a' , 'an', 'but', 'of', 'for', 'at', 'by', 'from']
+    let array = [];
+    let sentence = string.split(" ");
+    for ( let n = 0; n <sentence.length; n++ ) {
+      if ( n == 0 ) {
+        array.push( this.capitalize( sentence[n] ) )
+      } else {
+        if (exeptions.includes( sentence[n] ) ) {
+          array.push( sentence[ n ] )
+        } else {
+          array.push( this.capitalize( sentence[n] ) )
+        }
+      }
+
+    }
+    return array.join( " " );
+  }
 }
